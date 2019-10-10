bc5b8a0380d9f2366c208c5899b12bba9245b83c
blelump <blelump@martex.nom.pl>
2019-10-10 21:16:27 +0200

    Extract parser logic in order to create integration test

---
+++ spec/integration/big_parser_spec.rb
@@ -0,0 +1,28 @@
+require 'big_parser'
+require "json"
+
+RSpec.describe BigParser do
+  let(:topic)  { described_class.new(filename) }
+
+  let(:filename) { File.join(SPEC_ROOT, 'shared/example.csv') }
+
+  describe '#call' do
+    context 'when valid CSV file is provided' do
+      it 'generates valid output' do
+        topic.call
+
+        audit_overview_schema_base = JSON.load(
+          File.open(File.join(LIB_ROOT, 'output', 'AuditOverview.json'))
+        )
+
+        expect(audit_overview_schema_base).to include(
+          'attributes',
+          "@context"=>"https://odca.tech/v1",
+          "name"=>"AuditOverview",
+          "type"=>"spec/schame_base/1.0",
+          'pii_attributes' => include('auditReportNumber', 'auditReportOwner')
+        )
+      end
+    end
+  end
+end
