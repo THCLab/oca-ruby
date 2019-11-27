fb21fd30be33e3486fcd7fa07d4b026553dc026a
Marcin <marcin@opensoftware.pl>
2019-11-27 11:57:12 +0100

    Generate hashlink from ugly json

--- spec/unit/hashlink_generator_spec.rb
+++ spec/unit/hashlink_generator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Odca::HashlinkGenerator do
     context 'when hash is provided' do
       it 'generates hashlink string' do
         expect(described_class.call(test: 'test'))
-          .to eql('9VhMK68B5BMrMZ4kDpiA7UQvheEvSCwkvi6vDcKcyKTb')
+          .to eql('5cZ6S9VWSRPUcyYak416m51EhtJGfhSrfhuzdJsxiWxf')
       end
     end
   end
